9d17bea4d8e5c854112d42e837644608a5a774cc
BlueManiac <bluemaniac@users.noreply.github.com>
2023-02-16 11:06:25 +0100

    Add core-js to be able to use more modern javascript methods.

--- src/Web/Util/Client/array.ts
+++ src/Web/Util/Client/array.ts
@@ -1,12 +1,2 @@
-﻿export { }
-declare global {
-  interface Array<T> {
-    toSorted(lambda: (item: T) => any): Array<T>
-  }
-}
-
-Array.prototype.toSorted = function (lambda) {
-  return this.slice(0).sort((a, b) =>
-    lambda(a) > lambda(b) ? 1 : lambda(a) < lambda(b) ? -1 : 0
-  )
-}
+﻿import "core-js/actual/array/group"
+import "core-js/actual/array/to-sorted"
