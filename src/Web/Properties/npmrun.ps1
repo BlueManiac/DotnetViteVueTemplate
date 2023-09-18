132ee5e69916cf09d28d45a90fc09df8c62c9333
BlueManiac <bluemaniac@users.noreply.github.com>
2023-09-18 16:01:38 +0200

    Show vite and dotnet watch in split terminal inside vscode on F5

--- src/Web/Properties/npmrun.ps1
+++ src/Web/Properties/npmrun.ps1
@@ -1,4 +1,10 @@
-﻿$currentHash = (Get-FileHash '.\package.json').Hash
+﻿$projectParent = (get-item $PSScriptRoot).Parent
+$projectPath = $projectParent.FullName
+
+Set-Location $projectPath
+Clear-Host
+
+$currentHash = (Get-FileHash '.\package.json').Hash
 $oldHash = (Get-Content '.\node_modules\hash' -First 1 -ErrorAction SilentlyContinue | Out-String).Trim()
 
 if ($currentHash -ne $oldHash) {
