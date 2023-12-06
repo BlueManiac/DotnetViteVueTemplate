5737311f292e71d66d9079d4b5d1fb49422e65bd
BlueManiac <bluemaniac@users.noreply.github.com>
2023-12-06 16:31:21 +0100

    fetch: Add basic request interception

--- src/Web/Util/Client/fetch.ts
+++ src/Web/Util/Client/fetch.ts
@@ -37,7 +37,7 @@ export const fetch = async (url: RequestInfo | URL, init: RequestInitExtended) =
   if (!response.ok) {
     const contentType = response.headers.get('content-type')
     
-    const error = new Error( `${init.method ?? 'GET'} ${response.url} ${response.status}`)
+    const error = new Error(`${init.method ?? 'GET'} ${response.url} ${response.status}`)
     
     const problemDetails = contentType?.indexOf('application/problem+json') >= 0 && await response.json()
 
@@ -58,54 +58,56 @@ const parseResponse = async<T>(response: Response) => {
     return response.json() as T
   }
 
-  return response.text()
+  return response.text() as T
 }
 
-export const useApi = ({ apiUrl }) => {
+type RequestInterceptor = (init: RequestInitExtended) => RequestInitExtended
+
+export const useApi = ({ apiUrl, intercept = x => x }: { apiUrl: string, intercept?: RequestInterceptor}) => {
   return {
     url: apiUrl,
-    fetch: (url: RequestInfo | URL, init: RequestInitExtended = {}) => fetch(apiUrl + url, init),
+    fetch: (url: RequestInfo | URL, init: RequestInitExtended = {}) => fetch(apiUrl + url, intercept(init)),
     get: async <T>(url: RequestInfo | URL, init?: RequestInitExtended) => {
-      const response = await fetch(apiUrl + url, {
+      const response = await fetch(apiUrl + url, intercept({
         method: 'GET',
         ...init
-      })
+      }))
 
       return await parseResponse<T>(response)
     },
     post: async <T>(url: RequestInfo | URL, body?: any, init?: RequestInitExtended) => {
-      const response = await fetch(apiUrl + url, {
+      const response = await fetch(apiUrl + url, intercept({
         method: 'POST',
         body: JSON.stringify(body),
         headers: {
           'Content-Type': 'application/json'
         },
         ...init
-      })
+      }))
 
       return await parseResponse<T>(response)
     },
     put: async <T>(url: RequestInfo | URL, body?: any, init?: RequestInitExtended) => {
-      const response = await fetch(apiUrl + url, {
+      const response = await fetch(apiUrl + url, intercept({
         method: 'PUT',
         body: JSON.stringify(body),
         headers: {
           'Content-Type': 'application/json'
         },
         ...init
-      })
+      }))
 
       return await parseResponse<T>(response)
     },
     delete: async <T>(url: RequestInfo | URL, body?: any, init?: RequestInitExtended) => {
-      const response = await fetch(apiUrl + url, {
+      const response = await fetch(apiUrl + url, intercept({
         method: 'DELETE',
         body: JSON.stringify(body),
         headers: {
           'Content-Type': 'application/json'
         },
         ...init
-      })
+      }))
 
       return await parseResponse<T>(response)
     }
